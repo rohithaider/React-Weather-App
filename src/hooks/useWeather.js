e3946617847e7afa16c4fa8a98ec395ef670575c
rohithaider <rohithaider@gmail.com>
2024-11-05 14:06:25 +0600

    initializing useWeather custom hook for getting weather data from the OpenWeather API

---
+++ src/hooks/useWeather.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+const useWeather = ()=>{
+
+const [weatherData, setWeatherData] = useState({
+    location:"",
+    climate:"",
+    temperature:"",
+    maxTemperature:"",
+    minTemperature:"",
+    humidity:"",
+    cloudPercentage:"",
+    wind:"",
+    time:"",
+})
+
+
+}
