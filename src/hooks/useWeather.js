c13af4ecbe4b66791af4b8d47046c71a471c71fc
rohithaider <rohithaider@gmail.com>
2024-11-05 14:46:05 +0600

    use weather hook implemented and calling the api with api key, making the key private

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -1,17 +1,71 @@
-import { useState } from "react"
-const useWeather = ()=>{
+import { useState } from "react";
+const useWeather = () => {
+  const [weatherData, setWeatherData] = useState({
+    location: "",
+    climate: "",
+    temperature: "",
+    maxTemperature: "",
+    minTemperature: "",
+    humidity: "",
+    cloudPercentage: "",
+    wind: "",
+    time: "",
+    longitude: "",
+    latitude: "",
+  });
+  // creating this loading for making it true when api is calling
+  const [loading, setLoading] = useState({
+    state: false,
+    message: "",
+  });
+  //state for maintaining error
+  const [error, setError] = useState(null);
 
-const [weatherData, setWeatherData] = useState({
-    location:"",
-    climate:"",
-    temperature:"",
-    maxTemperature:"",
-    minTemperature:"",
-    humidity:"",
-    cloudPercentage:"",
-    wind:"",
-    time:"",
-})
+  //main function for api calling
 
+  const fetchWeatherData = async (latitude, longitude) => {
+    try {
+      setLoading({
+        ...loading,
+        state: true,
+        message: "Fetching weather data",
+      });
 
-}
+      //fetch call
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric`
+      );
+      if (!response.ok) {
+        //response.ok gets from the fetch call
+        const errorMessage = `Fetching weather data failed:${response.status}`;
+        throw new Error(errorMessage);
+      }
+
+      const data = await response.json(); //getting the data out from the fetch promise
+      //mapped weather data by specific selection with optional chaining
+      const updatedWeatherData = {
+        ...weatherData,
+        location: data?.name,
+        climate: data?.weather[0]?.main,
+        temperature: data?.main?.temp,
+        maxTemperature: data?.main?.temp_max,
+        minTemperature: data?.main?.temp_min,
+        humidity: data?.main?.humidity,
+        cloudPercentage: data?.clouds?.all,
+        wind: data?.wind?.speed,
+        time: data?.dt,
+        longitude: longitude,
+        latitude: latitude, //longitude and latitude are already coming from the fetchWeatherData's Parameter
+      };
+    } catch (err) {
+      setError(err);
+    } finally {
+      //calling in the finally because it is guaranteed to be executed after try or catch block
+      setLoading({
+        ...loading,
+        state: false,
+        message: "",
+      });
+    }
+  };
+};
