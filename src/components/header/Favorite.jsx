7372e855b00d5facab4a4576ba61b220be37086a
rohithaider <rohithaider@gmail.com>
2024-11-06 01:15:42 +0600

    Implemented add to favorites modal

--- src/components/header/Favorite.jsx
+++ src/components/header/Favorite.jsx
@@ -1,9 +1,9 @@
 import HeartIcon from '../../assets/assets/heart.svg'
-export default function Favorite(){
+export default function Favorite({onShow}){
     return (
         <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
 					<img src={HeartIcon}alt="" />
-					<span>Favorite Locations</span>
+					<span onClick={onShow}>Favorite Locations</span>
 				</div>
     );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,12 @@ import Favorite from "./Favorite";
 import FavoriteListModal from "./FavoriteListModal";
 import Logo from "./Logo";
 import Search from "./Search";
+import { useState } from "react";
 
 export default function Header() {
+
+  const [showFavModal,setShowFavModal] = useState(false);
+
   return (
     <header className="fixed w-full top-0 z-50 bg-gradient-to-b from-black/60 to-black/0 pb-10">
       <nav className="container flex items-center justify-between py-6">
@@ -11,8 +15,8 @@ export default function Header() {
 
         <div className="flex items-center gap-4 relative">
           <Search />
-          <Favorite />
-          <FavoriteListModal />
+          <Favorite onShow={()=>setShowFavModal(!showFavModal)} />
+          {showFavModal && <FavoriteListModal />}
         </div>
       </nav>
     </header>
