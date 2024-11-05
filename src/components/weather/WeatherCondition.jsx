1f167551b131cde304ddf9b7d2abb043d3b21e61
rohithaider <rohithaider@gmail.com>
2024-11-05 17:55:36 +0600

    Dynamically updating weather data completed

--- src/components/weather/WeatherCondition.jsx
+++ src/components/weather/WeatherCondition.jsx
@@ -3,44 +3,48 @@ import TempMin from '../../assets/assets/icons/temp-min.svg'
 import Humidity from '../../assets/assets/icons/humidity.svg'
 import Cloud from '../../assets/assets/icons/cloud.svg'
 import Wind from '../../assets/assets/icons/wind.svg'
+import { useContext } from 'react';
+import { WeatherContext } from '../../context'
 
 export default function WeatherCondition(){
+	const {weatherData} = useContext(WeatherContext);
+	const {maxTemperature,minTemperature,humidity,cloudPercentage,wind,climate} = weatherData
     return (
         <div>
-							<p className="text-sm lg:text-lg font-bold uppercase mb-8">thunderstorm with light drizzle</p>
+							<p className="text-sm lg:text-lg font-bold uppercase mb-8">The climate is <u>{climate}</u></p>
 							<ul className="space-y-6 lg:space-y-6">
 								<li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
 									<span>Temp max</span>
 									<div className="inline-flex space-x-4">
-										<p>19°</p>
+										<p>{Math.round(maxTemperature)}°</p>
 										<img src={TempMax} alt="temp-max" />
 									</div>
 								</li>
 								<li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
 									<span>Temp min</span>
 									<div className="inline-flex space-x-4">
-										<p>19°</p>
+										<p>{Math.round(minTemperature)}°</p>
 										<img src={TempMin} alt="temp-min" />
 									</div>
 								</li>
 								<li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
 									<span>Humidity</span>
 									<div className="inline-flex space-x-4">
-										<p>58%</p>
+										<p>{humidity}%</p>
 										<img src={Humidity} alt="humidity" />
 									</div>
 								</li>
 								<li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
 									<span>Cloudy</span>
 									<div className="inline-flex space-x-4">
-										<p>86%</p>
+										<p>{cloudPercentage}%</p>
 										<img src={Cloud} alt="cloudy" />
 									</div>
 								</li>
 								<li className="text-sm lg:text-lg flex items-center justify-between space-x-4">
 									<span>Wind</span>
 									<div className="inline-flex space-x-4">
-										<p>5km/h</p>
+										<p>{wind} km/h</p>
 										<img src={Wind} alt="wind" />
 									</div>
 								</li>
--- src/components/weather/WeatherHeadline.jsx
+++ src/components/weather/WeatherHeadline.jsx
@@ -2,13 +2,43 @@ import Pin from "../../assets/assets/pin.svg";
 import { useContext } from "react";
 import { WeatherContext } from "../../context";
 import { getFormatDate } from "../../utils/date-util";
+//importing icons
+import CloudIcon from "../../assets/assets/cloud.svg";
+import HazeIcon from "../../assets/assets/haze.svg";
+import SnowIcon from "../../assets/assets/icons/snow.svg";
+import SunnyIcon from "../../assets/assets/icons/sunny.svg";
+import RainyIcon from "../../assets/assets/rainy.svg";
+import ThunderIcon from "../../assets/assets/thunder.svg";
 export default function WeatherHeadline() {
   const { weatherData } = useContext(WeatherContext); // taking out the weather data value from the context
-  const {climate,location, temperature,time} = weatherData
+  const { climate, location, temperature, time } = weatherData;
+
+  function getWeatherIcon(climate) {
+    switch (climate) {
+      case "Rain":
+        return RainyIcon;
+      case "Clouds":
+        return CloudIcon;
+      case "Clear":
+        return SunnyIcon;
+      case "Snow":
+        return SnowIcon;
+      case "Thunder":
+        return ThunderIcon;
+      case "Fog":
+        return HazeIcon;
+      case "Haze":
+        return HazeIcon;
+      case "Mist":
+        return HazeIcon;
+	default:
+		return SunnyIcon;
+    }
+  }
   return (
     <div>
       <div className="max-md:flex items-center justify-between md:-mt-10">
-        <img src="./assets/cloud.svg" alt="cloud" />
+        <img src={getWeatherIcon(climate)}alt="cloud" />
         <div className="max-md:flex items-center max-md:space-x-4">
           <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
             {Math.round(temperature)}°
@@ -19,7 +49,10 @@ export default function WeatherHeadline() {
           </div>
         </div>
       </div>
-      <p className="text-sm lg:text-lg">{getFormatDate(time,"time",false)}-{getFormatDate(time,"date",false)}</p>
+      <p className="text-sm lg:text-lg">
+        {getFormatDate(time, "time", false)}-
+        {getFormatDate(time, "date", false)}
+      </p>
     </div>
   );
 }
